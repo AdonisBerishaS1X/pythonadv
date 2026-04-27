e03d5d916e24cd2491b35423570cbdb71c927443
AdonisBerishaS1X <rubingducks@gmail.com>
2026-04-27 19:19:43 +0200

    Lesson 1

---
+++ pythonProject/Listat.py
@@ -0,0 +1,27 @@
+emrat=["DANTE","LUCIEN","MEPHISTON","NICODEMUS","MATANEO","DANTE"]
+
+#print(emrat)
+
+#print(emrat[3])
+
+#emrat.append("FOR SANGUINIUS")
+
+print(emrat)
+
+#emrat.clear()
+
+print(emrat)
+
+print(emrat.count("DANTE"))
+
+emrat.reverse()
+
+print(emrat)
+
+emrat.insert(__index=1,__object="DANTE")
+print(emrat)
+
+print(emrat.index("DANTE"))
+
+emrat.remove("LUCIEN")
+print(emrat)
---
+++ pythonProject/Name.py
@@ -0,0 +1,19 @@
+name ="Dante wuz here"
+
+print(name)
+
+x = 7
+y = 4
+
+print(x-y)
+
+number = "10"
+
+konvertimi = int (number)
+
+print(type(x))
+print(type(name))
+
+numriFloat = "11.1111111"
+
+print(numriFloat)
